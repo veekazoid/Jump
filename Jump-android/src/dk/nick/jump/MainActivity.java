929106950745d20a1f6fa53f21bd5f2779776514
veekazoid <veekazoid@gmail.com>
2013-10-20 14:41:52 +0200

    first commit

---
+++ Jump-android/src/dk/nick/jump/MainActivity.java
@@ -0,0 +1,18 @@
+package dk.nick.jump;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        
+        initialize(new Jump(), cfg);
+    }
+}
