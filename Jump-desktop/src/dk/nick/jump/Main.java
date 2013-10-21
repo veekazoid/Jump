451fd27a3d59b7dc95d815150591f7c9611f73dd
veekazoid <veekazoid@gmail.com>
2013-10-21 07:54:30 +0200

    splashscreen added

--- Jump-desktop/src/dk/nick/jump/Main.java
+++ Jump-desktop/src/dk/nick/jump/Main.java
@@ -1,15 +1,16 @@
 package dk.nick.jump;
 
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "Jump";
+		cfg.title = "Jump " + Jump.VERSION;
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 1080;
+		cfg.height = 720;
 		
 		new LwjglApplication(new Jump(), cfg);
 	}
---
+++ Jump/src/dk/nick/jump/Screens/MainMenu.java
@@ -0,0 +1,49 @@
+package dk.nick.jump.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
+
+import dk.nick.jump.Jump;
+
+public class MainMenu implements Screen{
+
+	Jump game;
+	
+	public MainMenu(Jump game)
+	{
+		this.game = game;
+	}
+	
+	
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void show() {
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+}
